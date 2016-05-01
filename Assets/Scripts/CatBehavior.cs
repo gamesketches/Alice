9d18a67b2a9de7665fa270992dd292edf029d3fd
gamesketches <sam.von.ehren@gmail.com>
2016-05-01 18:59:18 -0400

    First pass on cat behavior, cat just follows you around

---
+++ Assets/Scripts/CatBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatBehavior : MonoBehaviour {
+
+	private Transform destination;
+	private NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+		destination = GameObject.FindGameObjectWithTag("Player").transform;
+		agent = GetComponent<NavMeshAgent>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination(destination.position);
+	}
+}
