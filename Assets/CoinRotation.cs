35a614de612d619a8e05ae106444c9c770d5c095
gamesketches <sam.von.ehren@gmail.com>
2016-05-01 18:39:18 -0400

    Added rotation to coins to make them a little more eye catching

---
+++ Assets/CoinRotation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinRotation : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.RotateAround(gameObject.transform.position, Vector3.up, 1f);
+	}
+}
