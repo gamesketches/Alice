4321abb3a821eaf11975942d78c3abe8e4f0657d
gamesketches <sam.von.ehren@gmail.com>
2016-05-02 16:20:10 -0400

    Putting piggy bank in, not getting destroyed yet but I'll figure that out later

--- Assets/Scripts/PiggyBankScript.cs
+++ Assets/Scripts/PiggyBankScript.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class PiggyBankScript : MonoBehaviour {
 
+	public GameObject Bulb;
 	public float fallSpeed = 1;
 	private bool falling = true;
 
 	public IEnumerator Fall() {
+		Destroy(Bulb);
 		while(falling){
 			Vector3 oldPos = gameObject.transform.position;
 			oldPos.y -= fallSpeed * Time.deltaTime;
