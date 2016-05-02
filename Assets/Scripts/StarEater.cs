35ac1fcddfeb4c7f0caf2f24ef1014863acc8383
gamesketches <sam.von.ehren@gmail.com>
2016-05-02 19:41:34 -0400

    Player now grows when eating a star

---
+++ Assets/Scripts/StarEater.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarEater : MonoBehaviour {
+
+	public float growBy = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray PsychicRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+		if(Physics.Raycast(PsychicRay, out hit, 100f)){
+			Debug.Log(hit.collider.gameObject.name);
+			if(hit.collider.gameObject.tag == "CandyStar" && Input.GetKeyDown(KeyCode.Space)) {
+				Vector3 scale = gameObject.transform.localScale;
+				scale += new Vector3(growBy, growBy, growBy);
+				gameObject.transform.localScale = scale;
+			}
+		}
+		Debug.DrawRay(PsychicRay.origin, PsychicRay.direction * 100f);
+	}
+}
