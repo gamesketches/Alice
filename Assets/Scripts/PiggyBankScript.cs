d79cd9ad83401e14bc0f71cd85240af778b95cb7
gamesketches <sam.von.ehren@gmail.com>
2016-05-02 17:16:40 -0400

    Added in shattered piggy bank, but the shards are flying weird places for some reason, need to debug. In the meantime, the piggy bank instantiates some coins on the ground

--- Assets/Scripts/PiggyBankScript.cs
+++ Assets/Scripts/PiggyBankScript.cs
@@ -6,6 +6,10 @@ public class PiggyBankScript : MonoBehaviour {
 	public GameObject Bulb;
 	public float fallSpeed = 1;
 	private bool falling = true;
+/*  Have this in for testing only
+	void Start() {
+		StartCoroutine(Fall());
+	}*/
 
 	public IEnumerator Fall() {
 		Destroy(Bulb);
@@ -18,7 +22,13 @@ public class PiggyBankScript : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
+		Debug.Log("trigger entered");
 		falling = false;
+		Vector3 shatteredPosition = gameObject.transform.position;
+		shatteredPosition.y += 5f;
+		Instantiate(Resources.Load<GameObject>("prefabs/PiggyBankShattered"),shatteredPosition, Quaternion.identity);
+		Instantiate(Resources.Load<GameObject>("prefabs/cookieCoin"), gameObject.transform.position, Quaternion.Euler(new Vector3(0, 0, 90)));
+		Instantiate(Resources.Load<GameObject>("prefabs/milkCoin"), gameObject.transform.position, Quaternion.Euler(new Vector3(0, 0, 90)));
 		Destroy(gameObject);
 	}
 }
