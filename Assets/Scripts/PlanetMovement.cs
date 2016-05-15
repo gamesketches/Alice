cbf9f91d6dda098595edb184c146b3a35b774a6f
gamesketches <sam.von.ehren@gmail.com>
2016-05-15 18:46:18 -0400

    Exposed player movement speed and lowered it slightly

--- Assets/Scripts/PlanetMovement.cs
+++ Assets/Scripts/PlanetMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlanetMovement : MonoBehaviour {
 
 	public FauxGravityAttractor attractor;
+	public float speed;
 	Rigidbody rb;
 	// Use this for initialization
 	void Start () {
@@ -21,7 +22,7 @@ public class PlanetMovement : MonoBehaviour {
 			return;
 		}
 
-		rb.MovePosition(rb.position + Camera.main.transform.TransformDirection(new Vector3(hori, 0, vert).normalized * 8.0f * Time.deltaTime));
+		rb.MovePosition(rb.position + Camera.main.transform.TransformDirection(new Vector3(hori, 0, vert).normalized * speed * Time.deltaTime));
 
 		attractor.Attract(transform);
 	}
