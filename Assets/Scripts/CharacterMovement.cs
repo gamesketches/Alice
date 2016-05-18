3ede8e7ec603da08482bdaa6ddcc3325f9b0f3f8
gamesketches <sam.von.ehren@gmail.com>
2016-05-17 23:10:13 -0400

    Fixed a lot of the issues with the bounce

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -7,9 +7,10 @@ using UnityEngine.VR;
 public class CharacterMovement : MonoBehaviour {
 
 	CharacterController controller;
-	public int jumpDelay = 60;
-	public float hopDistance = 1.0f;
+	public int jumpDelay = 120;
+	public float hopDistance = 0.1f;
 	private int stepCounter = 0;
+	private float baseHeight;
 	private bool hopping;
 	List<GameObject> furniture;
 	List<Vector3> furnitureStartPositions;
@@ -19,8 +20,8 @@ public class CharacterMovement : MonoBehaviour {
 		hopping = false;
 		VRSettings.renderScale = 0.5f;
 		controller = GetComponent<CharacterController>();
+		baseHeight = controller.height;
 		var goArray = FindObjectsOfType(typeof(GameObject));
-		Debug.Log(goArray.Length);
 		furniture = new List<GameObject>();
 		furnitureStartPositions = new List<Vector3>();
 		furnitureTargetPositions = new List<Vector3>();
@@ -39,13 +40,15 @@ public class CharacterMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		// Comment out this line to enable groundshake
+		//hopping = true;
 		float hori = Input.GetAxis("Horizontal");
 		float vert = Input.GetAxis("Vertical");
 
 		controller.Move(Camera.main.transform.forward * vert * Time.deltaTime * 10.0f + Physics.gravity);
 		transform.Rotate(0f, hori * 2f, 0f);
 
-		if(vert != 0f) {
+		if(vert != 0f && controller.height > baseHeight) {
 			stepCounter++;
 			if(stepCounter > jumpDelay && !hopping) {
 				StartCoroutine(FurnitureHop());
@@ -74,9 +77,9 @@ public class CharacterMovement : MonoBehaviour {
 		}
 		while(t >= 0) {
 			for(int i = 0; i < furniture.Count; i++) {
-				furniture[i].transform.position = Vector3.Lerp(furnitureTargetPositions[i],
-					furnitureStartPositions[i],
-											t);
+				furniture[i].transform.position = Vector3.Lerp(furnitureStartPositions[i],
+					furnitureTargetPositions[i],
+					t);
 			}
 			t -= Time.deltaTime * 5;
 			yield return null;
