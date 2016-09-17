f3fddbafc2aa5edd950857cc7377d9ba97478818
gamesketches <sam.von.ehren@gmail.com>
2016-09-17 17:17:49 -0400

    added intro scene to build, added a fade in for main scene

---
+++ Assets/fadeInScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class fadeInScene : MonoBehaviour {
+
+	Image blackOverlay;
+	// Use this for initialization
+	void Start () {
+		blackOverlay = GetComponent<Image>();
+		StartCoroutine(FadeIn());
+	}
+
+	IEnumerator FadeIn(){
+		float t = 0;
+		Color[] imageColors = new Color[] {blackOverlay.color, blackOverlay.color};
+		imageColors[1].a = 0;
+		while(t < 1) {
+			blackOverlay.color = Color.Lerp(imageColors[0], imageColors[1], t);
+			t += Time.deltaTime / 2;
+			yield return null;
+		}
+	}
+}
